a6ebb936d04132d5d6f540e9d6dfde7fb260561a
Brandon Leafman <17711952+Daxo@users.noreply.github.com>
2020-12-14 16:52:43 -0600

    Refactors hook to use useReducer

--- src/screens/usePokemonAPI.jsx
+++ src/screens/usePokemonAPI.jsx
@@ -1,35 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+
+const pokemonApiReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, isError: false };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        data: action.payload,
+        image: action.payload.sprites?.other?.dream_world?.front_default,
+        isLoading: false,
+        isError: false,
+      };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, isError: true };
+    default:
+      throw new Error();
+  }
+};
 
 export const usePokemonAPI = (defualtPokemon) => {
   const [pokemon, setPokemon] = useState(defualtPokemon);
-  const [data, setData] = useState({});
-  const [image, setImage] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+
+  const [state, dispatch] = useReducer(pokemonApiReducer, {
+    isLoading: false,
+    isError: false,
+    data: {},
+    image: undefined,
+  });
 
   useEffect(() => {
     fetchPokemon();
 
     async function fetchPokemon() {
+      dispatch({ type: "FETCH_INIT" });
+
       const API_PREIX = "https://pokeapi.co/api/v2/pokemon/";
       const url = new URL(`${API_PREIX}${pokemon}`);
-
-      setIsError(false);
-      setIsLoading(true);
 
       try {
         const result = await fetch(url);
         const data = await result.json();
-        setData(data);
 
-        setImage(data?.sprites?.other?.dream_world?.front_default);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (e) {
-        setIsError(true);
+        dispatch({ type: "FETCH_FAILURE" });
       }
-
-      setIsLoading(false);
     }
   }, [pokemon]);
 
-  return [{ data, image, isLoading, isError }, setPokemon];
+  return [{ ...state }, setPokemon];
 };
